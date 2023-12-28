2c1176042893bdd76225dc5b733f63c93dc32c09
Deep <pateldeep0989@gmail.com>
2023-12-28 17:20:42 -0500

    fix bugs1

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 require('./src/DB/conn')
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json'); // Replace with the path to your service account key
+// const serviceAccount = require('../serviceAccountKey.json'); // Replace with the path to your service account key
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://jobguru-972cd-default-rtdb.firebaseio.com', // Replace with your Firebase project URL
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://jobguru-972cd-default-rtdb.firebaseio.com', // Replace with your Firebase project URL
+// });
 const AuthRouter = require('./src/Routers/Auth')
 const app = express()
 const port = process.env.PORT || 3000;

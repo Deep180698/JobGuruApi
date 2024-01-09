91a1f096d3af9f9ffe48a51b483d528973948946
Deep <pateldeep0989@gmail.com>
2024-01-09 16:17:13 -0500

    change node module

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const express = require('express')
 require('./src/DB/conn')
-const admin = require('firebase-admin');
-// const serviceAccount = require('../serviceAccountKey.json'); // Replace with the path to your service account key
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: 'https://jobguru-972cd-default-rtdb.firebaseio.com', // Replace with your Firebase project URL
-// });
 const AuthRouter = require('./src/Routers/Auth')
 const app = express()
 const port = process.env.PORT || 3000;
-const path = require('path');
 
 app.use('/src/Images', express.static(__dirname + '/src/Images'));
 

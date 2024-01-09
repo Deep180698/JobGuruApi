6a4881fbe51fb1fcc42f42db3cfd9341b1c482de
Deep <pateldeep0989@gmail.com>
2024-01-09 17:20:57 -0500

    DB change

--- src/DB/conn.js
+++ src/DB/conn.js
@@ -2,5 +2,6 @@ const express = require('express')
 
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/JobGuru?directConnection=true")
+mongoose.connect("mongodb://jobguruapi.onrender.com:27017/JobGuru?directConnection=true")
+// mongoose.connect("mongodb://localhost:27017/JobGuru?directConnection=true")
 

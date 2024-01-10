84b8cff0c129e90ae479ac64b2eea3760945f016
Deep <pateldeep0989@gmail.com>
2024-01-10 02:14:34 -0500

    update Database

--- src/DB/conn.js
+++ src/DB/conn.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 const mongoose = require('mongoose');
 
-// mongoose.connect("mongodb://localhost:27017/JobGuru?directConnection=true")
+mongoose.connect("mongodb+srv://pateldeep0989:DPaRdx0RBe5h1ccB@jobguru.wrblo87.mongodb.net/")
 
-mongoose.connect('mongodb://127.0.0.1/JobGuru?directConnection=true')
+// mongoose.connect('mongodb://127.0.0.1/JobGuru?directConnection=true')
+
+// DPaRdx0RBe5h1ccB
+

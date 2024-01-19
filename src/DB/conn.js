c8ef3b1f354ccb14661e781f9c67522c8a35a31e
Deep <pateldeep0989@gmail.com>
2024-01-18 19:59:39 -0500

    Remove all files for a fresh start

--- src/DB/conn.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-require('dotenv').config();
-
-const mongoose = require('mongoose');
-
-// mongoose.connect("mongodb://localhost:27017/JobGuru")
-mongoose.connect(process.env.DB_URL)
-
